31bf383c4543b177b98122b57832907b0497dc8c
xuejr <()>
2020-03-11 15:37:58 +0800

    记录列表按时间倒序

--- cloudfunctions/getRecords/index.js
+++ cloudfunctions/getRecords/index.js
@@ -18,7 +18,7 @@ exports.main = async (event, context) => {
   // 承载所有读操作的 promise 的数组
   const tasks = []
   for (let i = 0; i < batchTimes; i++) {
-    const promise = db.collection('records').where({ uid: event.uid }).skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+    const promise = db.collection('records').where({ uid: event.uid }).orderBy('date', 'desc').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
     tasks.push(promise)
   }
   // 等待所有
