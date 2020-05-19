e671ab2fa5f921e547e6600b544e64bf2632ed1d
xuejr <()>
2020-05-19 11:29:08 +0800

    发现详情页

--- miniprogram/pages/find/detail.js
+++ miniprogram/pages/find/detail.js
@@ -6,14 +6,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list
+    detail: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      detail: list[options.index]
+    })
   },
 
   /**
@@ -27,7 +29,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
@@ -43,10 +45,4 @@ Page({
   onUnload: function () {
 
   },
-  openDetail (e) {
-    const { dataset } = e.currentTarget
-    wx.navigateTo({
-      url: '/pages/find/detail?index=' + dataset.index
-    })
-  }
 })
